34e8b6c8c0114cfc93ff3d27c20cd4690c8d08a3
skppnsk3 <bryanttruong0918@gmail.com>
2025-09-24 03:38:24 -0500

    Lots of Stuff

--- client/src/pages/Banner.jsx
+++ client/src/pages/Banner.jsx
@@ -4,13 +4,24 @@ import "../styles/components.scss";
 
 const Banner = () => {
   return (
-    <div className="banner">
-      <div className="banner-title">UH Student Event Planner</div>
-      <nav className="banner-nav">
-        <Link to="/profile" className="banner-link">Profile</Link>
-        <Link to="/volunteer-history" className="banner-link">Volunteer History</Link>
+    <>
+      {/* Red banner*/}
+      <div className="banner_red">
+        UH Student Event Planner
+      </div>
+
+      {/* White banner*/}
+      <div className="banner_white">
+        {/*Add white banner links here*/}
+        <div>Home</div>
+        <div>Events</div>
+        <div>Login</div>
+        <nav className="banner-nav">
+          <Link to="/profile" className="banner-link">Profile</Link>
+          <Link to="/volunteer-history" className="banner-link">Volunteer History</Link>
       </nav>
-    </div>
+      </div>
+    </>
   );
 };
 
