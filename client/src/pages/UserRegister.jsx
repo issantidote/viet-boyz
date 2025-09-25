ce314f10d93cec0eba0f6c12b70fb4732b88b8f3
skppnsk3 <bryanttruong0918@gmail.com>
2025-09-25 02:53:06 -0500

    Password Checking

--- client/src/pages/UserRegister.jsx
+++ client/src/pages/UserRegister.jsx
@@ -1,8 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/components.scss";
 import Banner from "./Banner";
 
 const UserRegister = () => {
+  
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const requirements = [
+    {
+      label: "At least 8 characters",
+      test: (pw) => pw.length >= 8,
+    },
+    {
+      label: "At least 1 digit",
+      test: (pw) => /\d/.test(pw),
+    },
+    {
+      label: "At least 1 special character (except <, >, &, \\, \", ', (, ), [, ], {, }, ;)",
+      test: (pw) =>
+        /[!@#$%^*_\-+=:,.?/|~`]/.test(pw),
+    },
+    {
+      label: "Passwords match",
+      test: (pw) => pw.length > 0 && pw === confirmPassword,
+    },
+  ];
+
+  const forbiddenChars = /[<>&"'()\[\]{};\\]/;
+
+  const getStatusIcon = (isValid) =>
+    isValid ? (
+      <span style={{ color: "green", marginRight: "8px" }}>✔</span>
+    ) : (
+      <span style={{ color: "red", marginRight: "8px" }}>✘</span>
+    );
+  
   return (
     <div className="form-page">
       <Banner />
@@ -25,13 +58,55 @@ const UserRegister = () => {
             </div>
 
             <div className="form-group">
-                <label htmlFor="register-password" className="form-label">Password</label>
-                <input type="password" id="register-password" className="form-input" />
+              <label htmlFor="register-password" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                id="register-password"
+                className="form-input"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+
+            {/* PASSWORD REQUIREMENTS */}
+            <div className="password-requirements">
+              {requirements.map((req, index) => {
+                const valid = !forbiddenChars.test(password) && req.test(password);
+                return (
+                  <div key={index} className="requirement-item">
+                    <div className="requirement-icon">
+                      {getStatusIcon(valid)}
+                    </div>
+                    <div className={`requirement-text ${valid ? "valid" : "invalid"}`}>
+                      {req.label}
+                    </div>
+                  </div>
+                );
+              })}
+
+              {forbiddenChars.test(password) && (
+                <div className="requirement-item">
+                  <div className="requirement-icon">✘</div>
+                  <div className="requirement-text invalid">
+                    Contains forbidden character(s): &lt;, &gt;, &, \, ", ', (, ), [, ], {`{ }`}, ;
+                  </div>
+                </div>
+              )}
             </div>
 
             <div className="form-group">
-                <label htmlFor="confirm-password" className="form-label">Confirm Password</label>
-                <input type="password" id="confirm-password" className="form-input" />
+              <label htmlFor="confirm-password" className="form-label">
+                Confirm Password
+              </label>
+              <input
+                type="password"
+                id="confirm-password"
+                className="form-input"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
             </div>
 
             <button type="submit" className="form-button">Register</button>
