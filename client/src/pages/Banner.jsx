e4d42528da5b384668a765a0a3e8beee3b175a46
skppnsk3 <bryanttruong0918@gmail.com>
2025-10-17 16:02:11 -0500

    front end fixes

--- client/src/pages/Banner.jsx
+++ client/src/pages/Banner.jsx
@@ -5,15 +5,6 @@ import "../styles/components.scss";
 const Banner = () => {
   return (
     <>
-    <div className="banner">
-      <div className="banner-title">UH Student Event Planner</div>
-      <nav className="banner-nav">
-        <Link to="/profile" className="banner-link">Profile</Link>
-        <Link to="/volunteer-history" className="banner-link">Volunteer History</Link>
-        <Link to='/notifications' className="banner-link">Notifications</Link>
-      </nav>
-    </div>
-
       {/* Red banner*/}
       <div className="banner_red">
         UH Student Event Planner
